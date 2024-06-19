267adb5d66bcad88d97c549a1983abb047098588
ap12-code <ap12@ap12.net>
2024-06-19 13:46:38 +0900

    a

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,35 +4,176 @@
     import "bootstrap-icons/font/bootstrap-icons.css";
     import "./global.css";
     import { onMount } from "svelte";
+    import _ from "lodash";
 
     let fs = false;
     let shown_sidebar = false;
 
-    const data: Record<string, string> = {
-        "/tool/manifests": "manifest.json",
-        "/tool/pack-mcmeta": "pack.mcmeta",
-        "/tool/ip": "IPチェッカー",
-        "/tool/lookup": "dig&nslookup",
-        "/tool/whois": "whois",
-        "/tool/ping": "ping",
-        "/tool/base64": "Base64 エンコーダー&デコーダー",
-        "/tool/bases": "基数変換",
-        "/tool/password": "パスワード生成",
-        "/tool/random": "ランダム整数生成",
-        "/tool/unicode": "Unicodeエスケープシーケンス",
-        "/tool/upper-lower": "大文字小文字変換",
-        "/tool/url": "URLエンコーダー&デコーダー",
-        "/tool/uuid": "UUID生成",
-        "/tool/unix-timestamp": "UNIX TIMESTAMP 変換",
-        "/tool/text-counter": "文字数カウンタ",
-        "/tool/counter": "カウンタ",
-        "/tool/clock": "時計",
-        "/tool/qrcode": "QRコード",
-        "/tool/mcsrvstat": "MCサーバーステータス",
-        "/tool/html": "HTMLエンコーダー&デコーダー",
-        "/tool/stopwatch": "ストップウォッチ",
-        "/tool/image": "画像形式変換",
-    };
+    type Tool = { href: string; name: string; icon: string };
+    type Tools = Tool[];
+    type Category = { icon: string; collapsed: boolean; name: string; tools: Tools }[];
+    type CategorySaved = { collapsed: boolean }[];
+
+    let data: Category = [
+        {
+            name: "変換ツール",
+            icon: "",
+            tools: [
+                {
+                    href: "/tool/bases",
+                    name: "基数",
+                    icon: "nut-fill",
+                },
+                {
+                    href: "/tool/upper-lower",
+                    name: "大文字小文字",
+                    icon: "type",
+                },
+                {
+                    href: "/tool/unix-timestamp",
+                    name: "UNIX TIMESTAMP",
+                    icon: "clock-fill",
+                },
+                {
+                    href: "/tool/image",
+                    name: "画像変換ツール",
+                    icon: "image-fill",
+                },
+            ],
+            collapsed: false,
+        },
+        {
+            name: "生成ツール",
+            icon: "",
+            tools: [
+                {
+                    href: "/tool/uuid",
+                    name: "UUID",
+                    icon: "info-lg",
+                },
+                {
+                    href: "/tool/password",
+                    name: "パスワード",
+                    icon: "asterisk",
+                },
+                {
+                    href: "/tool/random",
+                    name: "ランダム整数",
+                    icon: "123",
+                },
+                {
+                    href: "/tool/qrcode",
+                    name: "QRコード",
+                    icon: "qr-code",
+                },
+                {
+                    href: "/tool/pack-mcmeta",
+                    name: "pack.mcmeta",
+                    icon: "boxes",
+                },
+                {
+                    href: "/tool/manifest-json",
+                    name: "manifest.json",
+                    icon: "filetype-json",
+                },
+            ],
+            collapsed: false,
+        },
+        {
+            name: "エンコーダー/デコーダー",
+            icon: "",
+            tools: [
+                {
+                    href: "/tool/base64",
+                    name: "base64",
+                    icon: "file-text",
+                },
+                {
+                    href: "/tool/url",
+                    name: "URL",
+                    icon: "slash-lg",
+                },
+                {
+                    href: "/tool/unicode",
+                    name: "UNICODEエスケープシーケンス",
+                    icon: "card-text",
+                },
+                {
+                    href: "/tool/html",
+                    name: "HTML",
+                    icon: "code",
+                },
+            ],
+            collapsed: false,
+        },
+        {
+            name: "ネットワーク",
+            icon: "",
+            tools: [
+                {
+                    href: "/tool/ip",
+                    name: "IPチェッカー",
+                    icon: "globe2",
+                },
+                {
+                    href: "/tool/lookup",
+                    name: "dig&nslookup",
+                    icon: "wrench",
+                },
+                {
+                    href: "/tool/whois",
+                    name: "whois",
+                    icon: "bricks",
+                },
+                {
+                    href: "/tool/ping",
+                    name: "ping",
+                    icon: "activity",
+                },
+                {
+                    href: "/tool/mcsrvstat",
+                    name: "MCサーバーステータス",
+                    icon: "box-fill",
+                },
+            ],
+            collapsed: false,
+        },
+        {
+            name: "カウンタ",
+            icon: "",
+            tools: [
+                {
+                    href: "/tool/counter",
+                    name: "カウンタ",
+                    icon: "file-text-fill",
+                },
+                {
+                    href: "/tool/text-counter",
+                    name: "文字数カウンタ",
+                    icon: "file-text-fill",
+                },
+            ],
+            collapsed: false,
+        },
+        {
+            name: "時間",
+            icon: "",
+            tools: [
+                {
+                    href: "/tool/clock",
+                    name: "時計",
+                    icon: "clock",
+                },
+                {
+                    href: "/tool/stopwatch",
+                    name: "ストップウォッチ",
+                    icon: "stopwatch",
+                },
+            ],
+            collapsed: false,
+        },
+    ];
+    let favorites: Tools = [];
 
     let sel = "...";
     function move() {
@@ -42,11 +183,44 @@
         shown_sidebar = !shown_sidebar;
         save();
     }
+    function switchFavorite(tool: Tool) {
+        if (favorites.findIndex((p) => p.name == tool.name) != -1) {
+            favorites.splice(
+                favorites.findIndex((p) => p.name == tool.name),
+                1,
+            );
+        } else {
+            favorites.push(tool);
+        }
+        favorites = favorites;
+        save();
+    }
+    function switchCategory(index: number) {
+        data[index].collapsed = !data[index].collapsed;
+        save();
+    }
     function save() {
         window.localStorage.setItem("shown_sidebar", shown_sidebar.toString());
+        let parsed: CategorySaved = [];
+        for (let i = 0; i < data.length; i++) {
+            parsed[i] = { collapsed: data[i].collapsed };
+        }
+        window.localStorage.setItem("sidebar_data", JSON.stringify(parsed));
+        window.localStorage.setItem("sidebar_favorites", JSON.stringify(favorites));
     }
     function load() {
         shown_sidebar = window.localStorage.getItem("shown_sidebar") == "true";
+        const data2 = window.localStorage.getItem("sidebar_data");
+        if (data2) {
+            const parsed = JSON.parse(data2) as CategorySaved;
+            for (let i = 0; i < data.length; i++) {
+                data[i].collapsed = parsed[i].collapsed;
+            }
+        }
+        const rawFavorites = window.localStorage.getItem("sidebar_favorites");
+        if (rawFavorites) {
+            favorites = JSON.parse(rawFavorites) as Tools;
+        }
     }
     onMount(load);
     afterNavigate(() => {
@@ -70,9 +244,49 @@
                         <a href="/" class:back={$page.url.pathname != "/"}>ToolBox</a>
                     </div>
                     <a class:selected={$page.url.pathname == "/"} class="sidebar-button" href="/">ホーム</a>
+                    {#each favorites as t}
+                        <div class="tool">
+                            <a class:selected={$page.url.pathname == t.href} class="sidebar-button" href={t.href}>
+                                <i class="bi bi-{t.icon}"></i>
+                                {t.name}
+                            </a>
+                            <button class="favorite-button" on:click={(_) => switchFavorite(t)}>
+                                {#if favorites.find((p) => p.name == t.name)}
+                                    <i class="bi bi-star-fill"></i>
+                                {:else}
+                                    <i class="bi bi-star"></i>
+                                {/if}
+                            </button>
+                        </div>
+                    {/each}
                     <hr />
-                    {#each Object.keys(data).toSorted((a, b) => a.charCodeAt(6) - b.charCodeAt(6)) as d}
-                        <a class:selected={$page.url.pathname == d} class="sidebar-button" href={d}>{data[d]}</a>
+                    {#each data as d, i}
+                        {#if !d.collapsed}
+                            <button on:click={(_) => switchCategory(i)} class="sidebar-button">
+                                <i class="bi bi-caret-up-fill"></i>
+                                {d.name}
+                            </button>
+                            {#each d.tools as t}
+                                <div class="tool mg-right">
+                                    <a class:selected={$page.url.pathname == t.href} class="sidebar-button" href={t.href}>
+                                        <i class="bi bi-{t.icon}"></i>
+                                        {t.name}
+                                    </a>
+                                    <button class="favorite-button" on:click={(_) => switchFavorite(t)}>
+                                        {#if favorites.find((p) => p.name == t.name)}
+                                            <i class="bi bi-star-fill"></i>
+                                        {:else}
+                                            <i class="bi bi-star"></i>
+                                        {/if}
+                                    </button>
+                                </div>
+                            {/each}
+                        {:else}
+                            <button on:click={(_) => switchCategory(i)} class="sidebar-button">
+                                <i class="bi bi-caret-down-fill"></i>
+                                {d.name}
+                            </button>
+                        {/if}
                     {/each}
                 </div>
             </div>
@@ -110,6 +324,9 @@
         100% {
             transform: translateX(-200px);
         }
+    }
+    .mg-right {
+        margin-left: 20px;
     }
     .copyright {
         font-size: 18px;
@@ -171,6 +388,34 @@
         padding: 5px 10px;
         display: block;
         border-radius: 5px;
+    }
+    .tool.sidebar-button {
+        margin-left: 15px;
+    }
+    .tool {
+        display: flex;
+    }
+    .tool a {
+        flex: 1;
+    }
+    .tool button {
+        cursor: pointer;
+        border: none;
+        color: #fff;
+        text-align: center;
+        border-radius: 5px;
+        background-color: #222;
+    }
+    .tool button:hover {
+        background-color: #666;
+    }
+    button.sidebar-button {
+        border: none;
+        font-size: 16px;
+        display: block;
+        width: 100%;
+        text-align: left;
+        background-color: #222;
     }
     .sidebar-button:hover {
         background-color: #666;
